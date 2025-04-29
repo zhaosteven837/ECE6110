30177a8e2fdecf23f1e814888c14af0ae5b1f1a4
zhaosteven <zhaosteven837@gmail.com>
2025-04-29 17:52:36 -0400

    Fixing script

--- runScripts.py
+++ runScripts.py
@@ -12,16 +12,16 @@ futures = []
 def runScript(nDevices, packetDelay, simulationTime, radius, packetSize, dataRate):
     returnThing = subprocess.run(['./ns3', 'run', 'scratch/Final_Project.cc --nDevices=' + nDevices + '--packetDelay=' + packetDelay + '--simulationTime=' + simulationTime + '--radius=' + radius + '--packetSize=' + packetSize + '--dataRate=' + dataRate], stdout=subprocess.PIPE)
     try:
-        ipc = "nDevices: " + nDevices + ", packetDelay: " + packetDelay + ", simulationTime: " + simulationTime + ", radius: " + radius + ", packetSize: " + packetSize + ", dataRate: " + dataRate + ", " + returnThing.stdout.decode('utf-8').splitlines()[0].split()[2] + ", " + returnThing.stdout.decode('utf-8').splitlines()[1].split()[4] + ", " + returnThing.stdout.decode('utf-8').splitlines()[1].split()[4] + "\n"
+        ipc = "nDevices: " + nDevices + ", packetDelay: " + packetDelay + ", simulationTime: " + simulationTime + ", radius: " + radius + ", packetSize: " + packetSize + ", dataRate: " + dataRate + ", " + returnThing.stdout.decode('utf-8').splitlines()[0].split()[2] + ", " + returnThing.stdout.decode('utf-8').splitlines()[1].split()[4] + ", " + returnThing.stdout.decode('utf-8').splitlines()[1].split()[5] + "\n"
     except:
-        print("error")
+        print("error on config: " + "nDevices: " + nDevices + ", packetDelay: " + packetDelay + ", simulationTime: " + simulationTime + ", radius: " + radius + ", packetSize: " + packetSize + ", dataRate: " + dataRate)
         ipc = "error"
     return ipc
 
 def test():
     return 1
 
-with open("dump.txt", "w"):
+with open("dump2.txt", "w"):
     pass
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
@@ -35,5 +35,5 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
                             futures.append(future)
 
     for future in concurrent.futures.as_completed(futures):
-        with open("dump.txt", "a") as myfile:
+        with open("dump2.txt", "a") as myfile:
             myfile.write(future.result())
